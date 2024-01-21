3ee27521b3cc295820e12fac9b850d73b470d9d1
reganbp <reganbp@gmail.com>
2024-01-20 22:18:57 -0500

    ADD -src files from old test repository

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import "./App.css";
+import Login from "./Login";
+import Home from "./Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <Banner />
+    //   </header>
+    // </div>
+    // <Fragment>
+    //   <Banner />
+    //   <Login />
+    // </Fragment>
+    <HashRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        {/* <Route index element={<Home />} /> */}
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} /> */}
+        {/* </Route> */}
+      </Routes>
+    </HashRouter>
   );
 }
 
