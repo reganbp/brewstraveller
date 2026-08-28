785762f0ada9533a4600a691491c0f2d312b8c03
reganbp <reganbp@gmail.com>
2026-08-28 13:30:03 -0400

    UPDATE - pwa icons

--- frontend-vue/vite.config.ts
+++ frontend-vue/vite.config.ts
@@ -19,12 +19,18 @@ export default defineConfig({
         orientation: 'portrait',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: '/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: '/pwa-512x512.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
